cc159400321da358fa1dbd057b82e4ec3f8942d9
HaQadosch Berraka <haqadosch@pm.me>
2019-05-04 21:43:20 +0100

    Euler Problems 1: Done.

--- Multiples of 3 and 5.js
+++ Multiples of 3 and 5.js
@@ -1,2 +1,19 @@
-let x = 3;
+const assert = require("assert").strict;
+/**
+ *  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+function multiples(limit) {
+  function inner(actual = 1, limit = 0, total = 0) {
+    if (limit <= actual) {
+      return total;
+    }
+    if (actual % 3 === 0 || actual % 5 === 0) {
+      return inner(actual + 1, limit, total + actual);
+    }
+    return inner(actual + 1, limit, total);
+  }
+  return inner(0, limit, 0);
+}
+assert.deepStrictEqual(multiples(1000), 233168);
 //# sourceMappingURL=Multiples of 3 and 5.js.map
--- Multiples of 3 and 5.ts
+++ Multiples of 3 and 5.ts
@@ -1,1 +1,20 @@
-let x = 3;
+const assert = require("assert").strict;
+/**
+ *  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+function multiples(limit: number): number {
+  function inner(actual = 1, limit = 0, total = 0): number {
+    if (limit <= actual) {
+      return total;
+    }
+
+    if (actual % 3 === 0 || actual % 5 === 0) {
+      return inner(actual + 1, limit, total + actual);
+    }
+    return inner(actual + 1, limit, total);
+  }
+  return inner(0, limit, 0);
+}
+
+assert.deepStrictEqual(multiples(1000), 233168);
